30a3a3941173a88bc42cc613babac87efb5e0be4
Progammer <programmer@nbadigitalworlds.com>
2023-10-31 16:24:44 +0700

    Update index.js

--- routes/aoc/index.js
+++ routes/aoc/index.js
@@ -6,8 +6,8 @@ const authAdmin = require('../../lib/auth.admin');
 
 router.get('/iata',auth, service.getIATA);
 
-router.post('/token', service.getToken);
-router.post('/admin/token',authAdmin, service.getToken);
+router.post('/token',auth, service.getToken);
+router.post('/admin/token', service.getToken);
 router.post('/ticketflight',auth, service.getFlightTicket);
 router.post('/ticketprice',auth, service.getPriceTicket);
 router.post('/booking',auth, service.getBooking);
