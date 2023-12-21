1b29f45e88e13901426c5f881b748e304d39d8f2
lsram22 <137274609+programmer-nba@users.noreply.github.com>
2023-12-21 09:30:20 +0700

    update

--- models/pos.models/preorder.shop.model.js
+++ models/pos.models/preorder.shop.model.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 
 const PreOrderShopSchema = new mongoose.Schema({
   poshop_invoice: { type: String, required: true },
-  // poshop_ref_full: { type: String, required: false, default: "ไม่มี" },
+  poshop_ref_full: { type: String, required: false, default: "ไม่มี" },
   poshop_shop_id: { type: String, required: true },
   poshop_detail: { type: Array, required: false, default: [] },
   poshop_total: { type: Number, required: true },
@@ -15,7 +15,7 @@ const PreOrderShopSchema = new mongoose.Schema({
   poshop_ref_short_id: { type: String, required: false, default: "ไม่มี" }, // อ้างอิงกรณียกเลิกบิล
   poshop_timestamp: { type: Date, required: false, default: Date.now() },
   poshop_employee: { type: String, required: false, default: "ไม่มี" },
-  // poshop_cutoff : {type: Boolean, required: false, default: false}
+  poshop_cutoff : {type: Boolean, required: false, default: false}
 });
 
 const PreOrderShop = mongoose.model("preorder_shop", PreOrderShopSchema);
@@ -23,7 +23,7 @@ const PreOrderShop = mongoose.model("preorder_shop", PreOrderShopSchema);
 const validate = (data) => {
   const schema = Joi.object({
     poshop_invoice: Joi.string().required().label("กรุณากรอกไอดีรายการด้วย"),
-    // poshop_ref_full: Joi.string().default("ไม่มี"),
+    poshop_ref_full: Joi.string().default("ไม่มี"),
     poshop_shop_id: Joi.string()
       .required()
       .label("กรุณากรอกไอดีร้านที่ทำรายการด้วย"),
@@ -37,7 +37,7 @@ const validate = (data) => {
     poshop_ref_short_id: Joi.string().default("ไม่มี"),
     poshop_timestamp: Joi.date().raw().default(Date.now()),
     poshop_employee: Joi.string().default("ไม่มี"),
-    // poshop_cutoff : Joi.boolean().default(true)
+    poshop_cutoff : Joi.boolean().default(true)
   });
   return schema.validate(data);
 };
